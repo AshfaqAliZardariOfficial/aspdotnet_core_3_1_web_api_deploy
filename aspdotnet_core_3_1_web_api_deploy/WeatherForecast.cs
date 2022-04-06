178c0d3d140087850983b6423640ce19b94b87fc
Ashfaq Ali Zardari <ashfaq@techsol360.com>
2022-04-06 15:59:34 +0500

    Asp.net Core 3.1 Web API empty project workflow

---
+++ aspdotnet_core_3_1_web_api_deploy/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace aspdotnet_core_3_1_web_api_deploy
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
